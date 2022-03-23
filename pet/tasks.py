c6eb6a0525b1d7f8af63e6c69b679cd2b5c6f2db
GryffindorLi <1697476145@qq.com>
2022-03-23 09:16:27 +0100

    fix bug: original label more than 2, randomly select 2 to make it run

--- pet/tasks.py
+++ pet/tasks.py
@@ -872,8 +872,10 @@ class MFTC_Processor(DataProcessor):
                     if row[l] == "1":
                         tmp.append(l)
                     if len(tmp) > 1:
-                        tmp = random.choice(tmp, no_labels)
-                string = " ".join(tmp)
+                        tmp = random.choices(tmp, no_labels)
+                        string = " ".join(tmp)
+                    else:
+                        string = tmp[0]
                 
                 text_a = row[MFTC_Processor.TEXT_A_COLUMN]
                 text_b = row[MFTC_Processor.TEXT_B_COLUMN] if MFTC_Processor.TEXT_B_COLUMN >= 0 else None
