6d73c72afc7b7b8f27daa9167ed92c0255a9fdd0
simran0413 <45099537+simran0413@users.noreply.github.com>
2022-03-29 16:35:30 +0200

    change patterns

--- pet/pvp.py
+++ pet/pvp.py
@@ -665,14 +665,14 @@ class MFTC_PVP(PVP):
         # can also be empty).
         if self.pattern_id == 0:
             # this corresponds to the pattern [MASK]: a b
-            return [text_a, "This is about", self.mask * no_labels], []
+            return [text_a, "This is about ", self.mask, "and", self.mask], []
         elif self.pattern_id == 1:
             # this corresponds to the pattern [MASK] News: a || (b)
-            return ["What is the next sentence about", text_a, self.mask * no_labels], []
+            return ["What is the next sentence about ", text_a, ". It is about ", self.mask, "and", self.mask], []
         elif self.pattern_id == 2:
-            return [text_a, "The previous is about", self.mask * no_labels], []
+            return [text_a, "The previous is about ", self.mask, "and", self.mask], []
         elif self.pattern_id == 3:
-            return ["What is this for", text_a, self.mask * no_labels], []
+            return ["What is this for ", text_a, ". It is for ", self.mask, "and", self.mask], []
         else:
             raise ValueError("No pattern implemented for id {}".format(self.pattern_id))
 
